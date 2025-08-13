5c014082ffb642bd320165b37b85f6aa8b86a7ea
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-08-13 22:43:00 +0530

    added solana wallet

---
+++ src/components/SolanaWallet.tsx
@@ -0,0 +1,34 @@
+import {useState } from "react";
+import {mnemonicToSeed} from "bip39";
+import {derivePath} from "ed25519-hd-key";
+import {Keypair} from "@solana/web3.js";
+import nacl from "tweetnacl";
+
+export function SolanaWallet({mnemonic}:{mnemonic:string}){
+  const [currentIndex,setCurrentIndex]=useState(0);
+  const [publicKeys,setPublicKeys]=useState<any[]>([]);
+
+  const addWallet=async()=>{
+    const seed=await mnemonicToSeed(mnemonic);
+    const path=`m/44'/501'/${currentIndex}'/0'`;
+    const derivedSeed=derivePath(path,seed.toString("hex")).key;
+    const secret=nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const keypair=Keypair.fromSecretKey(secret);
+
+    setCurrentIndex(currentIndex+1);
+    setPublicKeys([...publicKeys,keypair.publicKey]);
+
+  };
+  return(
+    <div>
+        <button onClick={addWallet}>
+         Add Wallet
+        </button>
+        {publicKeys.map((p,idx)=>{
+            <div key={idx}>
+                 {p.toBase58()}
+            </div>
+        })}
+    </div>
+  );
+}
