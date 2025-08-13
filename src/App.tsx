00c8fe1232f551d868686f542a34c682b9babaa9
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-08-13 20:33:26 +0530

    added mnemonic

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { generateMnemonic } from "bip39";
 
 
 function App() {
-
-
+  const [mnemonic, setMnemonic] = useState("");
+  const handleGenerate = async () => {
+    const mn = await generateMnemonic();
+    setMnemonic(mn);
+  };
   return (
-    <></>
+    <>
+<button onClick={handleGenerate}>Create Seed Phrase</button>
+<br/><br/>
+<input 
+type="text"
+value={mnemonic}
+readOnly
+// style={{width:"100%",padding:"10px"}}
+/>
+    </>
   )
 }
 
